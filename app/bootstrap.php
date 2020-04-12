5e70e7a141cd784996bfe5850b3eb65a42d504a1
Odole Kayode <kayode@patricia.com.ng>
2020-04-12 22:01:22 +0100

    Updates

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,4 +2,11 @@
 
 require __DIR__ .'/../vendor/autoload.php';
 
+$_SESSION['urls'] = [];
+
 require 'Route.php';
+
+if (isset($_SESSION['urls']) && !in_array($_SERVER['REQUEST_URI'],$_SESSION['urls']) && $_SERVER['REQUEST_URI'] !== '/') {	
+	$_SESSION['urls'] = [];
+	throw new Exception("This route ".$_SERVER['REQUEST_URI']." doesn't exist in your Application !");	
+}
